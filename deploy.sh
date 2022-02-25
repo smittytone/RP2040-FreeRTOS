c984c0f55cac05c72cd907e59dc324dc11691c9e
Tony Smith <smittytone@icloud.com>
2022-02-25 14:42:01 +0000

    Specify compiler, build type

--- deploy.sh
+++ deploy.sh
@@ -56,7 +56,7 @@ if [[ ${do_build} -eq 1 ]]; then
     if [[ ! -e "./build" ]]; then
         # No build folder? Then create it
         # and configure the build
-        cmake -S . -B build/
+        cmake -S . -B build/ -D "CMAKE_C_COMPILER:FILEPATH=$(which arm-none-eabi-gcc)" -D CMAKE_BUILD_TYPE:STRING=Release
         err=$?
     fi
     
