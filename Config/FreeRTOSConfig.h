a7e0d323c5741f507ad4175c103c976f068730b7
Tony Smith <smittytone@users.noreply.github.com>
2022-03-01 09:05:04 +0000

    Add comments; permit timeslicing

--- Config/FreeRTOSConfig.h
+++ Config/FreeRTOSConfig.h
@@ -36,7 +36,7 @@
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
 #define configUSE_TICKLESS_IDLE                 0
 #define configCPU_CLOCK_HZ                      133000000   // 133MHz for RP2040
-#define configTICK_RATE_HZ                      100
+#define configTICK_RATE_HZ                      100         // FreeRTOS beats per second
 #define configMAX_PRIORITIES                    5           // Max number of priority values (0-24)
 #define configMINIMAL_STACK_SIZE                128
 #define configMAX_TASK_NAME_LEN                 16
@@ -49,7 +49,7 @@
 #define configUSE_COUNTING_SEMAPHORES           0
 #define configQUEUE_REGISTRY_SIZE               10
 #define configUSE_QUEUE_SETS                    0
-#define configUSE_TIME_SLICING                  0
+#define configUSE_TIME_SLICING                  1           // Allow FreeRTOS to schedule
 #define configUSE_NEWLIB_REENTRANT              0
 #define configENABLE_BACKWARD_COMPATIBILITY     0
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
