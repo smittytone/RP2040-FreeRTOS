e96677f4961dd7da054dfbee294ccb79759fb1b6
Tony Smith <tsmith@korewireless.com>
2024-08-14 09:14:43 +0100

    Match with RPi version

--- Config/FreeRTOSConfig.h
+++ Config/FreeRTOSConfig.h
@@ -32,29 +32,33 @@
 #define xPortPendSVHandler      isr_pendsv
 #define xPortSysTickHandler     isr_systick
 
-#define configUSE_PREEMPTION                    1           // Allow tasks to be pre-empted
-#define configUSE_TIME_SLICING                  1           // Allow FreeRTOS to switch tasks at each tick
+/* Scheduler Related */
+#define configUSE_PREEMPTION                    1                   // Allow tasks to be pre-empted
+#define configUSE_TIME_SLICING                  1                   // Allow FreeRTOS to switch tasks at each tick
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
 #define configUSE_TICKLESS_IDLE                 0
-#define configCPU_CLOCK_HZ                      133000000   // 133MHz for RP2040
-#define configTICK_RATE_HZ                      1000        // FreeRTOS beats per second
-#define configMAX_PRIORITIES                    5           // Max number of priority values (0-24)
-#define configMINIMAL_STACK_SIZE                128
-#define configMAX_TASK_NAME_LEN                 16
+#define configCPU_CLOCK_HZ                      133000000           // 133MHz for RP2040
+#define configTICK_RATE_HZ                      (TickType_t)1000    // FreeRTOS beats per second
+#define configMAX_PRIORITIES                    24                  // Max number of priority values (0-24)
+#define configMINIMAL_STACK_SIZE                (configSTACK_DEPTH_TYPE)512
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
-#define configUSE_TASK_NOTIFICATIONS            1
+#define configMAX_TASK_NAME_LEN                 16
 #define configTASK_NOTIFICATION_ARRAY_ENTRIES   3
+
+/* Synchronization Related */
 #define configUSE_MUTEXES                       0
 #define configUSE_RECURSIVE_MUTEXES             0
 #define configUSE_COUNTING_SEMAPHORES           0
-#define configQUEUE_REGISTRY_SIZE               10
+#define configUSE_TASK_NOTIFICATIONS            1
 #define configUSE_QUEUE_SETS                    0
+#define configQUEUE_REGISTRY_SIZE               10
 #define configUSE_NEWLIB_REENTRANT              0
 #define configENABLE_BACKWARD_COMPATIBILITY     0
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
 
-#define configSTACK_DEPTH_TYPE                  uint16_t
+/* System */
+#define configSTACK_DEPTH_TYPE                  uint32_t
 #define configMESSAGE_BUFFER_LENGTH_TYPE        size_t      // Defaults to size_t for backward compatibility,
                                                             // but can be changed if lengths will always be less
                                                             // than the number of bytes in a size_t.
@@ -62,7 +66,8 @@
 /* Memory allocation related definitions. */
 #define configSUPPORT_STATIC_ALLOCATION         0
 #define configSUPPORT_DYNAMIC_ALLOCATION        1           // Get FreeRTOS to allocation task memory
-#define configAPPLICATION_ALLOCATED_HEAP        1
+#define configAPPLICATION_ALLOCATED_HEAP        0
+#define configTOTAL_HEAP_SIZE                   (128*1024)
 
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                     0
@@ -82,11 +87,16 @@
 
 /* Software timer related definitions. */
 #define configUSE_TIMERS                        1
-#define configTIMER_TASK_PRIORITY               3
+#define configTIMER_TASK_PRIORITY               (configMAX_PRIORITIES - 1)
 #define configTIMER_QUEUE_LENGTH                10
-#define configTIMER_TASK_STACK_DEPTH            256
+#define configTIMER_TASK_STACK_DEPTH            1024
+
+/* RP2040 specific */
+#define configSUPPORT_PICO_SYNC_INTEROP         1
+#define configSUPPORT_PICO_TIME_INTEROP         1
 
 /* Define to trap errors during development. */
+#include <assert.h>
 #define configASSERT( x )
 
 /* Optional functions - most linkers will remove unused functions anyway. */
@@ -94,19 +104,20 @@
 #define INCLUDE_uxTaskPriorityGet               1
 #define INCLUDE_vTaskDelete                     1
 #define INCLUDE_vTaskSuspend                    1
-#define INCLUDE_xResumeFromISR                  1
 #define INCLUDE_vTaskDelayUntil                 1
 #define INCLUDE_vTaskDelay                      1
 #define INCLUDE_xTaskGetSchedulerState          1
 #define INCLUDE_xTaskGetCurrentTaskHandle       1
-#define INCLUDE_uxTaskGetStackHighWaterMark     0
-#define INCLUDE_xTaskGetIdleTaskHandle          0
-#define INCLUDE_eTaskGetState                   0
-#define INCLUDE_xEventGroupSetBitFromISR        1
-#define INCLUDE_xTimerPendFunctionCall          0
-#define INCLUDE_xTaskAbortDelay                 0
-#define INCLUDE_xTaskGetHandle                  0
+#define INCLUDE_uxTaskGetStackHighWaterMark     1
+#define INCLUDE_xTaskGetIdleTaskHandle          1
+#define INCLUDE_eTaskGetState                   1
+#define INCLUDE_xTaskAbortDelay                 1
+#define INCLUDE_xTaskGetHandle                  1
 #define INCLUDE_xTaskResumeFromISR              1
+#define INCLUDE_xQueueGetMutexHolder            1
+#define INCLUDE_xResumeFromISR                  1
+#define INCLUDE_xEventGroupSetBitFromISR        1
+#define INCLUDE_xTimerPendFunctionCall          1
 
 /* A header file that defines trace macro can be included here. */
 
